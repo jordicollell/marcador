3ed4f80187c2c9accf78c512157a24a0f39d25db
jordicollell <78138546+jordicollell@users.noreply.github.com>
2022-03-11 09:09:34 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,40 @@
+function restar () {
+    marcador = marcador - 1
+    if (marcador == -1) {
+        marcador = 0
+        for (let index = 0; index < 2; index++) {
+            basic.clearScreen()
+            basic.pause(100)
+            basic.showNumber(marcador)
+            basic.pause(100)
+        }
+    } else {
+        basic.showNumber(marcador)
+    }
+}
 input.onButtonPressed(Button.A, function () {
-    marcador = marcador + 1
-    basic.showNumber(marcador)
+    sumar()
 })
 input.onButtonPressed(Button.AB, function () {
-    marcador = 0
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . # # # .
-        . . . . .
-        . . . . .
-        `)
-})
-input.onButtonPressed(Button.B, function () {
-    marcador = marcador - 1
+    marcador = marcador - 0
     basic.showNumber(marcador)
+})
+function sumar () {
+    marcador = marcador + 1
+    if (marcador == 10) {
+        marcador = 9
+        for (let index = 0; index < 2; index++) {
+            basic.clearScreen()
+            basic.pause(100)
+            basic.showNumber(marcador)
+            basic.pause(100)
+        }
+    } else {
+        basic.showNumber(marcador)
+    }
+}
+input.onButtonPressed(Button.B, function () {
+    restar()
 })
 let marcador = 0
 basic.showLeds(`
