c236a7588207cebd1e10f9f430b4341550c95a01
jordicollell <78138546+jordicollell@users.noreply.github.com>
2022-03-11 09:16:37 +0100

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 function restar () {
-    marcador = marcador - 1
+    marcador += -1
     if (marcador == -1) {
         marcador = 0
         for (let index = 0; index < 2; index++) {
@@ -16,11 +16,11 @@ input.onButtonPressed(Button.A, function () {
     sumar()
 })
 input.onButtonPressed(Button.AB, function () {
-    marcador = marcador - 0
+    marcador = 0
     basic.showNumber(marcador)
 })
 function sumar () {
-    marcador = marcador + 1
+    marcador += 1
     if (marcador == 10) {
         marcador = 9
         for (let index = 0; index < 2; index++) {
